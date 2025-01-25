be552ed8f34f015b3ffdb15a9bd7bdf4178d2e25
Tan Yanbo <tanyanbo95@hotmail.com>
2025-01-25 09:12:51 +0800

    type command

--- src/handleCommand.zig
+++ src/handleCommand.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 pub fn handler(input: []u8) !void {
-    const stdout = std.io.getStdOut().writer();
     var iter = std.mem.splitSequence(u8, input, " ");
 
     const command = iter.next();
@@ -19,8 +18,37 @@ pub fn handler(input: []u8) !void {
         const intExitCode = std.fmt.parseInt(u8, exitCode.?, 10) catch 5;
         std.process.exit(intExitCode);
     } else if (std.mem.eql(u8, command.?, "echo")) {
-        try stdout.print("{s}\n", .{input[5..]});
+        try handleEcho(input);
+    } else if (std.mem.eql(u8, command.?, "type")) {
+        try handleType(input);
     } else {
-        try stdout.print("{s}: command not found\n", .{input});
+        try handleUnknownCommands(input);
     }
+}
+
+fn handleType(input: []u8) !void {
+    const stdout = std.io.getStdOut().writer();
+    const builtins = [_][]const u8{ "echo", "type", "exit" };
+    var argsIter = std.mem.splitSequence(u8, input[5..], " ");
+
+    while (argsIter.next()) |arg| {
+        const res = for (builtins) |builtin| {
+            if (std.mem.eql(u8, builtin, arg)) {
+                break true;
+            }
+        } else false;
+        if (res) {
+            try stdout.print("{s} is a shell builtin\n", .{arg});
+        }
+    }
+}
+
+fn handleEcho(input: []u8) !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("{s}\n", .{input[5..]});
+}
+
+fn handleUnknownCommands(input: []u8) !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("{s}: command not found\n", .{input});
 }
