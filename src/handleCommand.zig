4f932b66ccffa84541cc81dffebd3d024dd838fb
Tan Yanbo <tanyanbo95@hotmail.com>
2025-01-25 08:34:08 +0800

    exit 0

--- src/handleCommand.zig
+++ src/handleCommand.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn handler(input: []u8) !void {
     const stdout = std.io.getStdOut().writer();
 
-    if (std.mem.eql(u8, input, "exit")) {
+    if (std.mem.eql(u8, input, "exit 0")) {
         std.process.exit(0);
         return;
     }
