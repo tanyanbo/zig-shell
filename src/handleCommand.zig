6e717b3cdab0be5caba4f3c3c285b01285468dc8
Tan Yanbo <tanyanbo95@hotmail.com>
2025-01-25 08:45:54 +0800

    echo

--- src/handleCommand.zig
+++ src/handleCommand.zig
@@ -2,11 +2,22 @@ const std = @import("std");
 
 pub fn handler(input: []u8) !void {
     const stdout = std.io.getStdOut().writer();
+    var iter = std.mem.splitSequence(u8, input, " ");
 
-    if (std.mem.eql(u8, input, "exit 0")) {
-        std.process.exit(0);
+    const command = iter.next();
+
+    if (command == null) {
+        std.process.exit(1);
         return;
     }
 
-    try stdout.print("{s}: command not found\n", .{input});
+    if (std.mem.eql(u8, command.?, "exit")) {
+        const exitCode = iter.next();
+        std.process.exit(try std.fmt.parseInt(u8, exitCode.?, 10));
+        return;
+    } else if (std.mem.eql(u8, command.?, "echo")) {
+        try stdout.print("{s}\n", .{input[5..]});
+    } else {
+        try stdout.print("{s}: command not found\n", .{input});
+    }
 }
